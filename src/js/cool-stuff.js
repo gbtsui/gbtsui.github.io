18a382524ce33d9d09f231ab9124fdb903daa019
gbtsui <gabriel.tsui@protonmail.com>
2024-12-03 11:23:29 -0500

    Fixed links in cool-stuff.html

--- src/js/cool-stuff.js
+++ src/js/cool-stuff.js
@@ -1,3 +1,8 @@
-document.getElementById("cool-thing-fractals").addEventListener('click', function () {
-    window.location.href = "/src/cool-stuff/fractals.html";
-});
+document.addEventListener('click', function(event) {
+    // Check if the clicked element is one of our navigation links
+    if (event.target.matches('#cool-thing-fractals')) {
+        window.location.href = "/src/cool-stuff/fractals.html";
+    } else if (event.target.matches('#cool-thing-about-this-site')) {
+        window.location.href = "/src/cool-stuff/about-this-site.html";
+    }
+}, false);
